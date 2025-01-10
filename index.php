c7fef66e622bfdb73c575773619c53e99f7014ce
topschij <mtopschij@me.com>
2025-01-10 19:04:23 +1300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<html lang="en"><head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Just Bees</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+            background-color: #34B8FF;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            text-align: center;
+            box-sizing: border-box;
+        }
+
+        .content {
+            max-width: 90%;
+            padding-left: 20px;
+            padding-right: 20px;
+            box-sizing: border-box;
+        }
+
+        .logo {
+            width: 100%;
+            max-width: 600px;
+            height: auto;
+            margin-bottom: 20px;
+        }
+
+        .tagline {
+            font-size: 1.5em;
+            color: #ffffff;
+            font-weight: bold;
+        }
+
+        .footer {
+            position: absolute;
+            bottom: 10px;
+            font-size: 0.8em;
+            color: #ffffff;
+            text-align: center;
+            width: 100%;
+            padding-left: 20px;
+            padding-right: 20px;
+            box-sizing: border-box;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="content">
+        <img src="just-bees.png" alt="Just Bees Logo" class="logo">
+        <p class="tagline">Just friends, just family™</p>
+        <br>
+<a href="https://testflight.apple.com/join/bAD9VhSP" style="text-decoration: none; background-color: white; padding: 20px; border-radius: 15px; color: #34B8FF;"><strong style="padding: 30px; font-size: 25px;">Download Beta</strong></a>    </div>
+
+    <div class="footer">
+        ©. Copyright Just Bees® - All Rights Reserved. Just Bees is a registered trademark of South Limited.<br>
+        Creative Design by <a href="https://designbreak.studio" style="color: white; text-decoration: none;"><strong>Design Break</strong></a>
+
+    </div>
+
+
+
+</body></html>
